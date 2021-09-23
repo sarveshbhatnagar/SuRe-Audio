9ea44a286a9a68bba3b8d0c900b1f8afc506d583
Pratyashshrivastava <86169534+Pratyashshrivastava@users.noreply.github.com>
2021-09-24 00:39:22 +0530

    ...

--- audio_module/downloadaudio.py
+++ audio_module/downloadaudio.py
@@ -1,11 +1,30 @@
 import pafy
+import os
+
 
 url = "https://www.youtube.com/watch?v=dTu5dTEzVM4"
 def get_audio(url):
     video = pafy.new(url)
 
     bestaudio = video.getbestaudio()
-    return bestaudio.download()
-get_audio(url)
+    return bestaudio.download(filepath="../audio_files/"), bestaudio.filename
+outpt,filename=get_audio(url)
+
+def change_name(filename,newname):
+    with open("filenamewebm")
+
+
+    
+
+
+
+# def save_file(outpt):
+#     current_file= open("")
 
 # “../audio_files/filename.mp3” 
+
+
+# from pytube import YouTube
+# yt = YouTube("https://www.youtube.com/watch?v=n06H7OcPd-g")
+# yt = yt.get('mp4', '720p')
+# yt.download('../audio_files/yt.mp3')
